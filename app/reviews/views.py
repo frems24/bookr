87a8d9094b480d8637d4a6502a20f20f5d03dd85
Wojtek G <w20g16@gmail.com>
2022-10-08 17:34:05 +0200

    r01 First view and QueryDict

--- app/reviews/views.py
+++ app/reviews/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    name = request.GET.get("name") or "świecie"
+    return HttpResponse(f"Witaj, {name}!")
