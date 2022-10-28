031ae808e6598ec9ffc6fd32d2ae1888a111bd2d
Wojtek G <w20g16@gmail.com>
2022-10-28 21:16:22 +0200

    Add book list and details views

--- app/reviews/urls.py
+++ app/reviews/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('book-search', views.book_search),
+    path('', views.welcome_view, name='welcome_view'),
+    path('books/', views.book_list, name='book_list'),
+    path('books/<int:pk>/', views.book_detail, name='book_detail'),
 ]
